ab44b24c3a9b45bccc15db534018bd95d550de62
emmywise <adefila.emmywise@gmail.com>
2020-08-03 17:12:06 +0100

    activities.js

---
+++ activities.js
@@ -0,0 +1,25 @@
+var getRandomActivities = function() {
+    var url = `http://www.boredapi.com/api/activity`
+    
+    $.ajax(url)
+	.done(function( data ) {
+
+		var text = `activity: ${data.activity}<br></br>
+                    type: ${data.type}<br></br>
+                    participants: ${data.participants}<br></br>
+                    price: $ ${data.price}<br></br>
+                    link: ${data.link}<br></br>
+                    key: ${data.key}<br></br>
+                    accessibility: ${data.accessibility}`
+                                        
+        $("#data").html(text);
+    
+        console.log('data: ', data);
+    });
+
+};
+
+$("#search").click(function(){
+    //get link the api function to dispaly the random activities
+    getRandomActivities();
+});
